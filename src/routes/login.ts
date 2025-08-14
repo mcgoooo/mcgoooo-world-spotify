a2a0061664e9dc7b43b7a7cfaafefa5596b3c9fe
igberty@gmail.com <ahilton@odeko.com>
2025-08-14 16:55:17 +0100

    wip

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { setCookie } from "hono/cookie";
+import { html, raw } from 'hono/html';
 import { Env } from "../types/Env";
 
 import { getEnvKey } from "../utils/env";
@@ -9,20 +10,33 @@ import { loginUrl } from "../utils/urls";
 const scopes = ["user-read-currently-playing"].join(" ");
 
 export const loginRoute = new Hono<{ Bindings: Env }>();
+const cookieOptions: { secure: boolean } = { secure: true };
 
 loginRoute.get("/login", (c) => {
   const state = randomString(16);
   const returnTo = c.req.query('return_to');
-  console.log(returnTo,"returnTo");
-  if (returnTo) {
-      setCookie(c, "return_to", returnTo);
-  }
-  return c.redirect(
-    loginUrl(
+  const url =    loginUrl(
       getEnvKey(c, "CLIENT_ID"),
       getEnvKey(c, "REDIRECT_URI"),
       state,
       scopes,
-    ),
-  );
+    )
+
+    if (returnTo) {
+
+    setCookie(c, "return_to", returnTo, cookieOptions);
+  }
+
+   return c.html(
+    html`
+      <!doctype html>
+        <meta charset="utf-8">
+        <title>Redirecting to spotify login</title>
+        <script>
+          setTimeout(() => location.href = "${raw(url)}", 1);
+        </script>
+    `
+  )
+
+
 });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,17 +6,18 @@ import { Env } from "../types/Env";
 export const currentUserRoute = new Hono<{ Bindings: Env }>();
 
 currentUserRoute.get("/current-user", async (c) => {
-  const denied = c.json({ user: false }, 404);
+  const denied = (error: string) => c.json({ user: false, error }, 404);
   const token = getCookie(c, "spotify_access_token");
-  
-  if (!token) return denied
+
+  if (!token) return denied('token')
   try {
     
     const user = await spotifyUser(token);
-    if (!user) return denied;
+    if (!user) return denied('user');
     return c.json(user);
 
   } catch (error) {
-    return denied;
+    console.log(error)
+    return denied('error');
   }
 });

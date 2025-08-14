4245d9701d67dd5a6aacec2543b1bab982d120e3
igberty@gmail.com <ahilton@odeko.com>
2025-08-14 09:32:21 +0100

    add calls to get users playlists
    we had to deal with offsets in the spotify api.
    for this we need to get the first set of playlists,
    as that has the total amount of playlists.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Hono } from "hono";
 import { callbackRoute } from "./routes/callback";
 import { loginRoute } from "./routes/login";
 import { currentUserRoute } from "./routes/user";
+import { currentUserPlaylistsRoute } from "./routes/user/palylists";
 import { Env } from "./types/Env";
 
 const app = new Hono<{ Bindings: Env }>();
-[loginRoute, callbackRoute, currentUserRoute].forEach((route) => {
+[loginRoute, callbackRoute, currentUserRoute,currentUserPlaylistsRoute].forEach((route) => {
   app.route("/", route);
 });
 
---
+++ src/types/SpotifyUser/Playlist.ts
@@ -0,0 +1,44 @@
+export type SpotifyPlaylist = {
+  collaborative: boolean;
+  description: string;
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: {
+    height: number;
+    url: string;
+    width: number;
+  }[];
+  name: string;
+  owner: {
+    display_name: string;
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  primary_color: string | null;
+  public: boolean;
+  snapshot_id: string;
+  tracks: {
+    href: string;
+    total: number;
+  };
+  type: string;
+  uri: string;
+};
+
+export type SpotifyPlaylistResponse  = {
+  href: string;
+  limit: number;
+  next: string | null;
+  offset: number;
+  previous: string | null;
+  total: number;
+  items: SpotifyPlaylist[];
+};
